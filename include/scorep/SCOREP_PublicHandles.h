421e216790b708c970f5766db66ff6c3af84741b
Robert Schöne <rschoene@users.noreply.github.com>
2017-06-26 17:04:08 +0200

    Update SCOREP_PublicHandles.h

--- include/scorep/SCOREP_PublicHandles.h
+++ include/scorep/SCOREP_PublicHandles.h
@@ -34,58 +34,51 @@
 
 
 /**
- * @file
+ * @file       SCOREP_PublicHandles.h
  *
- *
+ * @brief Description of definition handles.
+ * This header defines an enumeration to map SCOREP_AnyHandle to specific handles.
+ * It also contains some of these handle definitions.
  */
-
 
 #include <scorep/SCOREP_PublicTypes.h>
 
 
 /**
- * Substrate events. List every event that is going to be used by the
- * substrate mechanism.
+ * handle types, lists all handle types that are supported.
  */
 typedef enum SCOREP_HandleType
 {
-    SCOREP_HANDLE_TYPE_ANY = 0,
-    SCOREP_HANDLE_TYPE_ATTRIBUTE,
-    SCOREP_HANDLE_TYPE_CALLING_CONTEXT,
-    SCOREP_HANDLE_TYPE_CALLPATH,
-    SCOREP_HANDLE_TYPE_COMMUNICATOR,
-    SCOREP_HANDLE_TYPE_GROUP,
-    SCOREP_HANDLE_TYPE_INTERIM_COMMUNICATOR,
-    SCOREP_HANDLE_TYPE_INTERIM_RMA_WINDOW,
-    SCOREP_HANDLE_TYPE_INTERRUPT_GENERATOR,
-    SCOREP_HANDLE_TYPE_LOCATION,
-    SCOREP_HANDLE_TYPE_LOCATION_GROUP,
-    SCOREP_HANDLE_TYPE_LOCATION_PROPERTY,
-    SCOREP_HANDLE_TYPE_METRIC,
-    SCOREP_HANDLE_TYPE_MPI_CARTESIAN_TOPOLOGY,
-    SCOREP_HANDLE_TYPE_MPI_CARTESIAN_COORDS,
-    SCOREP_HANDLE_TYPE_PARAMETER,
-    SCOREP_HANDLE_TYPE_PROPERTY,
-    SCOREP_HANDLE_TYPE_REGION,
-    SCOREP_HANDLE_TYPE_RMA_WINDOW,
-    SCOREP_HANDLE_TYPE_SAMPLING_SET,
-    SCOREP_HANDLE_TYPE_SAMPLING_SET_RECORDER,
-    SCOREP_HANDLE_TYPE_SOURCE_CODE_LOCATION,
-    SCOREP_HANDLE_TYPE_SOURCE_FILE,
-    SCOREP_HANDLE_TYPE_STRING,
-    SCOREP_HANDLE_TYPE_SYSTEM_TREE_NODE,
-    SCOREP_HANDLE_TYPE_SYSTEM_TREE_NODE_PROPERTY,
+    SCOREP_HANDLE_TYPE_ANY = 0,                   /** The handle type is not defined/invalid */
+    SCOREP_HANDLE_TYPE_ATTRIBUTE,                 /** The handle type is SCOREP_AttributeHandle (defined in src/measurement/definitions/include/SCOREP_DefinitionHandles.h) */
+    SCOREP_HANDLE_TYPE_CALLING_CONTEXT,           /** The handle type is SCOREP_CallingContextHandle */
+    SCOREP_HANDLE_TYPE_CALLPATH,                  /** The handle type is SCOREP_CallpathHandle */
+    SCOREP_HANDLE_TYPE_GROUP,                     /** The handle type is SCOREP_GroupHandle */
+    SCOREP_HANDLE_TYPE_INTERIM_COMMUNICATOR,      /** The handle type is SCOREP_InterimCommunicatorHandle */
+    SCOREP_HANDLE_TYPE_INTERRUPT_GENERATOR,       /** The handle type is SCOREP_InterruptGeneratorHandle */
+    SCOREP_HANDLE_TYPE_LOCATION,                  /** The handle type is SCOREP_LocationHandle */
+    SCOREP_HANDLE_TYPE_LOCATION_GROUP,            /** The handle type is SCOREP_LocationGroupHandle */
+    SCOREP_HANDLE_TYPE_LOCATION_PROPERTY,         /** The handle type is SCOREP_LocationPropertyHandle */
+    SCOREP_HANDLE_TYPE_METRIC,                    /** The handle type is SCOREP_MetricHandle (defined in SCOREP_PublicTypes.h) */
+    SCOREP_HANDLE_TYPE_PARADIGM,                  /** The handle type is SCOREP_ParadigmHandle (defined in SCOREP_PublicTypes.h) */
+    SCOREP_HANDLE_TYPE_PARAMETER,                 /** The handle type is SCOREP_ParameterHandle */
+    SCOREP_HANDLE_TYPE_PROPERTY,                  /** The handle type is SCOREP_PropertyHandle */
+    SCOREP_HANDLE_TYPE_REGION,                    /** The handle type is SCOREP_RegionHandle (defined in SCOREP_PublicTypes.h) */
+    SCOREP_HANDLE_TYPE_RMA_WINDOW,                /** The handle type is SCOREP_RmaWindowHandle */
+    SCOREP_HANDLE_TYPE_SAMPLING_SET,              /** The handle type is SCOREP_SamplingSetHandle (defined in SCOREP_PublicTypes.h) */
+    SCOREP_HANDLE_TYPE_SAMPLING_SET_RECORDER,     /** The handle type is SCOREP_SamplingSetRecorderHandle */
+    SCOREP_HANDLE_TYPE_SOURCE_CODE_LOCATION,      /** The handle type is SCOREP_SourceCodeLocationHandle */
+    SCOREP_HANDLE_TYPE_SOURCE_FILE,               /** The handle type is SCOREP_SourceFileHandle (defined in SCOREP_PublicTypes.h) */
+    SCOREP_HANDLE_TYPE_STRING,                    /** The handle type is SCOREP_StringHandle */
+    SCOREP_HANDLE_TYPE_SYSTEM_TREE_NODE,          /** The handle type is SCOREP_SystemTreeNodeHandle */
+    SCOREP_HANDLE_TYPE_SYSTEM_TREE_NODE_PROPERTY, /** The handle type is SCOREP_SystemTreeNodePropertyHandle */
 
-    SCOREP_HANDLE_TYPE_NUM_HANDLES
+    SCOREP_HANDLE_TYPE_NUM_HANDLES                /** Not ABI  */
 } SCOREP_HandleType;
-
-typedef SCOREP_AnyHandle SCOREP_AttributeHandle;
 
 typedef SCOREP_AnyHandle SCOREP_CallingContextHandle;
 
 typedef SCOREP_AnyHandle SCOREP_CallpathHandle;
-
-typedef SCOREP_AnyHandle SCOREP_CommunicatorHandle;
 
 typedef SCOREP_AnyHandle SCOREP_GroupHandle;
 
@@ -93,17 +86,11 @@ typedef SCOREP_AnyHandle SCOREP_InterimCommunicatorHandle;
 
 typedef SCOREP_AnyHandle SCOREP_InterruptGeneratorHandle;
 
-typedef SCOREP_AnyHandle SCOREP_InterimRmaWindowHandle;
-
 typedef SCOREP_AnyHandle SCOREP_LocationHandle;
 
 typedef SCOREP_AnyHandle SCOREP_LocationGroupHandle;
 
 typedef SCOREP_AnyHandle SCOREP_LocationPropertyHandle;
-
-typedef SCOREP_AnyHandle SCOREP_MPICartesianTopologyHandle;
-
-typedef SCOREP_AnyHandle SCOREP_MPICartesianCoordsHandle;
 
 typedef SCOREP_AnyHandle SCOREP_ParameterHandle;
 
@@ -120,20 +107,6 @@ typedef SCOREP_AnyHandle SCOREP_StringHandle;
 typedef SCOREP_AnyHandle SCOREP_SystemTreeNodeHandle;
 
 typedef SCOREP_AnyHandle SCOREP_SystemTreeNodePropertyHandle;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 #endif /* SCOREP_PUBLIC_HANDLES_H */
